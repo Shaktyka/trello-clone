c3e1e129413292a53c5fe6ed8791cafa924acc03
Shaktyka <k-i-s-u-n-j-a@list.ru>
2020-11-01 16:11:40 +0500

    Добавляет добавление пустых заметок в колонки

--- script.js
+++ script.js
@@ -3,6 +3,26 @@ let columnIdCounter = 4;
 
 const columns = document.querySelectorAll(`.column`);
 
+// Создаёт заметку
+const createNote = () => {
+    const noteEl = document.createElement(`div`);
+    noteEl.classList.add(`note`);
+    noteEl.setAttribute(`draggable`, `true`);
+    noteEl.setAttribute(`data-note-id`, noteIdCounter);
+    noteIdCounter++;
+    return noteEl;
+};
+
+// Обработчик клика по кнопке "Добавить заметку"
+const addNoteBtnClickHandler = (evt) => {
+    const target = evt.target;
+    const column = target.closest(`.column`);
+    const notesBlock = column.querySelector(`[data-notes]`);
+    const note = createNote();
+    notesBlock.appendChild(note);
+};
+
 columns.forEach((column) => {
-    const addNoteBtn = column.querySelector(`[data-action-addNote]`)
+    const addNoteBtn = column.querySelector(`[data-action-addNote]`);
+    addNoteBtn.addEventListener(`click`, addNoteBtnClickHandler);
 });
