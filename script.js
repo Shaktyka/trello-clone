b249b439f53c6f3eb26048f3f09458af8d455dba
Shaktyka <k-i-s-u-n-j-a@list.ru>
2020-11-01 15:53:33 +0500

    Добавляет немного семантики в вёрстку

---
+++ script.js
@@ -0,0 +1,8 @@
+let noteIdCounter = 8;
+let columnIdCounter = 4;
+
+const columns = document.querySelectorAll(`.column`);
+
+columns.forEach((column) => {
+    const addNoteBtn = column.querySelector(`[data-action-addNote]`)
+});
